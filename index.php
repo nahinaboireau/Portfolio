fc63bd95c8e9411278bfdd1dfdfeb18ec53cc40c
nahinaboireau <124175254+nahinaboireau@users.noreply.github.com>
2023-11-22 08:27:48 +0100

    :art:
    fin du début

--- index.php
+++ index.php
@@ -4,8 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NB</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- 1er police d'écriture  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Marcellus&display=swap" rel="stylesheet">
 </head>
+
 <body>
+<nav>
+    <img src="./images/logo-NB.png" alt="" class="logo">
+    <div class="rubrique"> 
+            <a href="">A propos</a> 
+            <a href="">Projets</a></a>
+            <a href="">Contact</a>    
+    </div>         
+</nav>
+
+
+<section class="accueil">
+    
+</section>
     
 </body>
 </html>
