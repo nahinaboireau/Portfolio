657ca6d7b37f619bb3d49c2c00be356f475fabb4
nahinaboireau <124175254+nahinaboireau@users.noreply.github.com>
2023-12-14 09:58:34 +0100

    :art:
    slider formata telephone fini

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector(".slider");
-    const slides = document.querySelectorAll(".slide");
-    let counter = 0;
+// document.addEventListener("DOMContentLoaded", function() {
+//     const slider = document.querySelector(".slider");
+//     const slides = document.querySelectorAll(".slide");
+//     let counter = 0;
   
-    function updateSlider() {
-      const transformValue = -counter * 537; //   largeur de chaque diapo
-      slider.style.transform = `translateX(${transformValue}px)`;
-    }
+//     function updateSlider() {
+//       const transformValue = -counter * 537; //   largeur de chaque diapo
+//       slider.style.transform = `translateX(${transformValue}px)`;
+//     }
   
-    function nextSlide() {
-      counter++;
-      if (counter === slides.length) {
-        counter = 0;
-      }
-      updateSlider();
-    }
+//     function nextSlide() {
+//       counter++;
+//       if (counter === slides.length) {
+//         counter = 0;
+//       }
+//       updateSlider();
+//     }
   
-    setInterval(nextSlide, 3000); // transition 3sec
-  });
+//     setInterval(nextSlide, 3000); // transition 3sec
+//   });
   
 
-  function scrollDown() {
-    // Utiliser la fonction scrollTo pour faire défiler vers le bas
-    window.scrollTo({
-        top: window.innerHeight,
-        behavior: 'smooth'
-    });
+//   function scrollDown() {
+//     // Utiliser la fonction scrollTo pour faire défiler vers le bas
+//     window.scrollTo({
+//         top: window.innerHeight,
+//         behavior: 'smooth'
+//     });
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
+  let counter = 0;
+
+  function updateSlider() {
+      const slideWidth = slides[0].offsetWidth; // Get the width of a single slide
+      const transformValue = -counter * slideWidth;
+      slider.style.transform = `translateX(${transformValue}px)`;
+  }
+
+  function nextSlide() {
+      counter++;
+      if (counter === slides.length) {
+          counter = 0;
+      }
+      updateSlider();
+  }
+
+  setInterval(nextSlide, 3000); // transition 3sec
+});
+
+function scrollDown() {
+  // Utiliser la fonction scrollTo pour faire défiler vers le bas
+  window.scrollTo({
+      top: window.innerHeight,
+      behavior: 'smooth'
+  });
 }
 
 
