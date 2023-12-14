7a4f3819345e1a2e254f61b3f722e1977faf1b66
nahinaboireau <124175254+nahinaboireau@users.noreply.github.com>
2023-12-14 10:39:57 +0100

    :art:
    modif

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const slider = document.querySelector(".slider");
-//     const slides = document.querySelectorAll(".slide");
-//     let counter = 0;
-  
-//     function updateSlider() {
-//       const transformValue = -counter * 537; //   largeur de chaque diapo
-//       slider.style.transform = `translateX(${transformValue}px)`;
-//     }
-  
-//     function nextSlide() {
-//       counter++;
-//       if (counter === slides.length) {
-//         counter = 0;
-//       }
-//       updateSlider();
-//     }
-  
-//     setInterval(nextSlide, 3000); // transition 3sec
-//   });
-  
-
-//   function scrollDown() {
-//     // Utiliser la fonction scrollTo pour faire défiler vers le bas
-//     window.scrollTo({
-//         top: window.innerHeight,
-//         behavior: 'smooth'
-//     });
-// }
-
 document.addEventListener("DOMContentLoaded", function () {
   const slider = document.querySelector(".slider");
   const slides = document.querySelectorAll(".slide");
